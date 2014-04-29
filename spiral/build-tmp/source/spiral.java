561c05ed6a8972fc5bcdc23f2967c6a735941484
aaa <a06kin@gmail.com>
2014-04-29 09:20:25 +0300

    save work to gif gif name - timestamp

--- spiral/build-tmp/source/spiral.java
+++ spiral/build-tmp/source/spiral.java
@@ -3,6 +3,8 @@ import processing.data.*;
 import processing.event.*; 
 import processing.opengl.*; 
 
+import gifAnimation.*; 
+
 import java.util.HashMap; 
 import java.util.ArrayList; 
 import java.io.File; 
@@ -13,6 +15,14 @@ import java.io.OutputStream;
 import java.io.IOException; 
 
 public class spiral extends PApplet {
+
+
+
+GifMaker gifExport;
+
+int fr = 0;
+
+int allFr = 256;
 
 float minT = 2.5f;
 float step = 0.01f;
@@ -29,6 +39,9 @@ public void setup() {
 	smooth();
 	background(255, 255, 255);
 	size(512, 512);
+
+	gifExport = new GifMaker(this, getDate() + ".gif", 100);
+	gifExport.setRepeat(0); // make it an "endless" animation
 }
 
 public void draw() {
@@ -46,6 +59,41 @@ public void draw() {
 	}
 	
 	minT+= step;
+
+	export();
+}
+
+public void export() {
+  if(fr < allFr) {
+    gifExport.setDelay(20);
+    gifExport.addFrame();
+    fr++;
+  } else {
+    gifExport.finish();
+    fr++;
+    println("gif saved");
+    exit();
+  }
+}
+
+public String getDate(){
+	int d = day();    // Values from 1 - 31
+  int mo = month();  // Values from 1 - 12
+  int y = year();   // 2003, 2004, 2005, etc.
+
+  String s = String.valueOf(d);
+  s += "." + String.valueOf(mo);
+  s += "." +String.valueOf(y);
+
+  int sec = second();  // Values from 0 - 59
+  int mi = minute();  // Values from 0 - 59
+  int h = hour();    // Values from 0 - 23
+
+  s += "_" + String.valueOf(h);
+  s += ":" + String.valueOf(mi);
+  s += ":" + String.valueOf(sec);
+
+  return s;
 }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "spiral" };
--- spiral/spiral.pde
+++ spiral/spiral.pde
@@ -1,3 +1,11 @@
+import gifAnimation.*;
+
+GifMaker gifExport;
+
+int fr = 0;
+
+int allFr = 256;
+
 float minT = 2.5;
 float step = 0.01;
 
@@ -13,6 +21,9 @@ void setup() {
 	smooth();
 	background(255, 255, 255);
 	size(512, 512);
+
+	gifExport = new GifMaker(this, getDate() + ".gif", 100);
+	gifExport.setRepeat(0); // make it an "endless" animation
 }
 
 void draw() {
@@ -30,4 +41,39 @@ void draw() {
 	}
 	
 	minT+= step;
+
+	export();
+}
+
+void export() {
+  if(fr < allFr) {
+    gifExport.setDelay(20);
+    gifExport.addFrame();
+    fr++;
+  } else {
+    gifExport.finish();
+    fr++;
+    println("gif saved");
+    exit();
+  }
+}
+
+String getDate(){
+	int d = day();    // Values from 1 - 31
+  int mo = month();  // Values from 1 - 12
+  int y = year();   // 2003, 2004, 2005, etc.
+
+  String s = String.valueOf(d);
+  s += "." + String.valueOf(mo);
+  s += "." +String.valueOf(y);
+
+  int sec = second();  // Values from 0 - 59
+  int mi = minute();  // Values from 0 - 59
+  int h = hour();    // Values from 0 - 23
+
+  s += "_" + String.valueOf(h);
+  s += ":" + String.valueOf(mi);
+  s += ":" + String.valueOf(sec);
+
+  return s;
 }
