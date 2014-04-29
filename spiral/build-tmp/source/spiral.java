9308777ed8b544b444c1a3c0e5173896825164d0
aaa <a06kin@gmail.com>
2014-04-29 15:48:34 +0300

    object box

--- spiral/build-tmp/source/spiral.java
+++ spiral/build-tmp/source/spiral.java
@@ -24,129 +24,154 @@ int fr = 0;
 
 int allFr = 256;
 
-float minT = 2.5f;
-float step = 0.01f;
-
-float a = 1;
-
-float alfa = 15.0f;
-
 float E = (float) Math.E;
 
-float b1 = 1.5f;
+Box boxx;
 
-Box first;
+Tourch t;
 
-Box[] other;
 
 public void setup() {
 	smooth();
-	background(255, 255, 255);
+	background(255);
 	size(512, 512);
 
 	gifExport = new GifMaker(this, getDate() + ".gif", 100);
 	gifExport.setRepeat(0); // make it an "endless" animation
 
-	first = new Box(0,0,20);
+	boxx = new Box(20, 255, 1, 1.5f, 2.5f, 0.01f, 90);
+	t = new Tourch();
 
-	other = new Box[24];
-
-	for (int i = 0; i < other.length; ++i) {
-		other[i] = new Box(0,0,20);
-	}
 }
+
 
 public void draw() {
 	noStroke();
 	rectMode(CORNER);
 	//fill(255, 10); // semi-transparent white
-	fill(255);
-  rect(0, 0, width, height);
+	fill(0);
+	rect(0, 0, width, height);
 
-  stroke(1);
+	stroke(255);
 
 	translate(height/2, width/2);
 
-	float r = pow(a * E, b1 * minT);
-	float x = r * cos(minT);
-	float y = r * sin(minT);
 
-	first.move(x,y);
-	first.display();
+	boxx.nextPath();
+	boxx.display();
 
-	for (int j = 0; j < other.length; ++j) {
-		float xs = x * cos(alfa * j) - y * sin(alfa * j);
-		float ys = x * sin(alfa * j) + y * cos(alfa * j);
-		other[j].move(xs,ys);
-		other[j].display();
-	}
-	
-	minT+= step;
+	t.display();
 
 	export();
 }
 
+
 public void export() {
-  if(fr < allFr) {
-    gifExport.setDelay(20);
-    gifExport.addFrame();
-    fr++;
-  } else {
-    gifExport.finish();
-    fr++;
-    println("gif saved");
-    exit();
-  }
+	if(fr < allFr) {
+		gifExport.setDelay(20);
+		gifExport.addFrame();
+		fr++;
+	} else {
+		gifExport.finish();
+		fr++;
+		println("gif saved");
+		exit();
+	}
 }
 
 public String getDate(){
 	int d = day();    // Values from 1 - 31
-  int mo = month();  // Values from 1 - 12
-  int y = year();   // 2003, 2004, 2005, etc.
+	int mo = month();  // Values from 1 - 12
+	int y = year();   // 2003, 2004, 2005, etc.
 
-  String s = String.valueOf(d);
-  s += "." + String.valueOf(mo);
-  s += "." +String.valueOf(y);
+	String s = String.valueOf(d);
+	s += "." + String.valueOf(mo);
+	s += "." +String.valueOf(y);
 
-  int sec = second();  // Values from 0 - 59
-  int mi = minute();  // Values from 0 - 59
-  int h = hour();    // Values from 0 - 23
+	int sec = second();  // Values from 0 - 59
+	int mi = minute();  // Values from 0 - 59
+	int h = hour();    // Values from 0 - 23
 
-  s += "_" + String.valueOf(h);
-  s += ":" + String.valueOf(mi);
-  s += ":" + String.valueOf(sec);
+	s += "_" + String.valueOf(h);
+	s += ":" + String.valueOf(mi);
+	s += ":" + String.valueOf(sec);
 
-  return s;
+	return s;
 }
 
 class Box { 
-  int c; //TODO: set color in constructor & use it
-  float size;
-  PVector data;
-  PVector speed;
+	int strokeS;
+	float size;
+	PVector data;
 
-  Box(float x, float y, float size){
-  	data = new PVector(x, y);
-  	this.size = size;
-  }
+	float a;  //= 1;
+	float b;  //= 1.5;
 
+	float r;
 
-  public void display() {
-  	if ((data.x > -width/2 && data.x < width/2) && (data.y > -height/2 && data.y < height/2)){
-	    rectMode(CENTER);
-	    rect(data.x,data.y,size,size);
-	  }else{
-	  	print("out");
-	  }
-  }
+	float step;  //= 0.01;
+	float now; //= 2.5;
 
-  public void move(float x, float y){
-  	if (data != null){
-  		data.x = x;
-  		data.y = y;
-  	}
-  }
-}
+	float angle;
+
+	Box(float size, int strokeS, float a, float b, float now, float step, float angle){
+		this.strokeS = strokeS;
+		this.size = size;
+		this.a = a;
+		this.b = b;
+		this.step = step;
+		this.now = now;
+		this.angle = angle;
+
+		this.r = pow(a * E, b * now);
+		float x = r * cos(now);
+
+		float y = r * sin(now);
+
+		this.data = new PVector(x, y);
+	}
+
+	public void display() {
+		//fill(c);
+		noFill();
+		stroke(strokeS);
+		rectMode(CENTER);
+
+		rect(data.x,data.y,size,size);
+	}	
+
+	public void nextPath(){
+		now += step;
+
+		r = pow(a * E, b * now);
+
+		float xs = r * cos(now);
+		float ys = r * sin(now);
+
+		data.x = xs * cos(angle) - ys * sin(angle);
+		data.y = xs * sin(angle) + ys * cos(angle);
+	}
+};
+
+class Tourch{
+
+	int inner_radius = 10;
+	int c_radius = 100;
+	int distance = 5;
+	float stroke_weight = 0.5f;
+
+	Tourch(){
+
+	}
+
+	public void display(){
+		noStroke();
+		for (int i = 0; i < c_radius; i+=distance){
+			fill(0xffFFFFFF, i/inner_radius);
+			ellipse(0, 0, c_radius - i, c_radius - i);
+		}
+	}
+};
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "spiral" };
     if (passedArgs != null) {
--- spiral/spiral.pde
+++ spiral/spiral.pde
@@ -6,126 +6,150 @@ int fr = 0;
 
 int allFr = 256;
 
-float minT = 2.5;
-float step = 0.01;
-
-float a = 1;
-
-float alfa = 15.0;
-
 float E = (float) Math.E;
 
-float b1 = 1.5;
+Box boxx;
 
-Box first;
+Tourch t;
 
-Box[] other;
 
 void setup() {
 	smooth();
-	background(255, 255, 255);
+	background(255);
 	size(512, 512);
 
 	gifExport = new GifMaker(this, getDate() + ".gif", 100);
 	gifExport.setRepeat(0); // make it an "endless" animation
 
-	first = new Box(0,0,20);
+	boxx = new Box(20, 255, 1, 1.5, 2.5, 0.01, 90);
+	t = new Tourch();
 
-	other = new Box[24];
-
-	for (int i = 0; i < other.length; ++i) {
-		other[i] = new Box(0,0,20);
-	}
 }
+
 
 void draw() {
 	noStroke();
 	rectMode(CORNER);
 	//fill(255, 10); // semi-transparent white
-	fill(255);
-  rect(0, 0, width, height);
+	fill(0);
+	rect(0, 0, width, height);
 
-  stroke(1);
-
+	stroke(255);
 	translate(height/2, width/2);
 
-	float r = pow(a * E, b1 * minT);
-	float x = r * cos(minT);
-	float y = r * sin(minT);
 
-	first.move(x,y);
-	first.display();
+	boxx.nextPath();
+	boxx.display();
 
-	for (int j = 0; j < other.length; ++j) {
-		float xs = x * cos(alfa * j) - y * sin(alfa * j);
-		float ys = x * sin(alfa * j) + y * cos(alfa * j);
-		other[j].move(xs,ys);
-		other[j].display();
-	}
-	
-	minT+= step;
+	t.display();
 
 	export();
 }
 
+
 void export() {
-  if(fr < allFr) {
-    gifExport.setDelay(20);
-    gifExport.addFrame();
-    fr++;
-  } else {
-    gifExport.finish();
-    fr++;
-    println("gif saved");
-    exit();
-  }
+	if(fr < allFr) {
+		gifExport.setDelay(20);
+		gifExport.addFrame();
+		fr++;
+	} else {
+		gifExport.finish();
+		fr++;
+		println("gif saved");
+		exit();
+	}
 }
 
 String getDate(){
 	int d = day();    // Values from 1 - 31
-  int mo = month();  // Values from 1 - 12
-  int y = year();   // 2003, 2004, 2005, etc.
+	int mo = month();  // Values from 1 - 12
+	int y = year();   // 2003, 2004, 2005, etc.
 
-  String s = String.valueOf(d);
-  s += "." + String.valueOf(mo);
-  s += "." +String.valueOf(y);
+	String s = String.valueOf(d);
+	s += "." + String.valueOf(mo);
+	s += "." +String.valueOf(y);
 
-  int sec = second();  // Values from 0 - 59
-  int mi = minute();  // Values from 0 - 59
-  int h = hour();    // Values from 0 - 23
+	int sec = second();  // Values from 0 - 59
+	int mi = minute();  // Values from 0 - 59
+	int h = hour();    // Values from 0 - 23
 
-  s += "_" + String.valueOf(h);
-  s += ":" + String.valueOf(mi);
-  s += ":" + String.valueOf(sec);
+	s += "_" + String.valueOf(h);
+	s += ":" + String.valueOf(mi);
+	s += ":" + String.valueOf(sec);
 
-  return s;
+	return s;
 }
 
 class Box { 
-  color c; //TODO: set color in constructor & use it
-  float size;
-  PVector data;
-  PVector speed;
+	int strokeS;
+	float size;
+	PVector data;
 
-  Box(float x, float y, float size){
-  	data = new PVector(x, y);
-  	this.size = size;
-  }
+	float a;  //= 1;
+	float b;  //= 1.5;
 
+	float r;
 
-  void display() {
-  	if ((data.x > -width/2 && data.x < width/2) && (data.y > -height/2 && data.y < height/2)){
-	    rectMode(CENTER);
-	    rect(data.x,data.y,size,size);
-	  }else{
-	  	println("out");
-	  }
-  }
+	float step;  //= 0.01;
+	float now; //= 2.5;
 
-  void move(float x, float y){
-  	if (data != null){
-  		data.x = x;
-  		data.y = y;
-  	}
-  }
-}
+	float angle;
+
+	Box(float size, int strokeS, float a, float b, float now, float step, float angle){
+		this.strokeS = strokeS;
+		this.size = size;
+		this.a = a;
+		this.b = b;
+		this.step = step;
+		this.now = now;
+		this.angle = angle;
+
+		this.r = pow(a * E, b * now);
+		float x = r * cos(now);
+
+		float y = r * sin(now);
+
+		this.data = new PVector(x, y);
+	}
+
+	void display() {
+		//fill(c);
+		noFill();
+		stroke(strokeS);
+		rectMode(CENTER);
+
+		rect(data.x,data.y,size,size);
+	}	
+
+	void nextPath(){
+		now += step;
+
+		r = pow(a * E, b * now);
+
+		float xs = r * cos(now);
+		float ys = r * sin(now);
+
+		data.x = xs * cos(angle) - ys * sin(angle);
+		data.y = xs * sin(angle) + ys * cos(angle);
+	}
+};
+
+class Tourch{
+
+	int inner_radius = 10;
+	int c_radius = 100;
+	int distance = 5;
+	float stroke_weight = 0.5;
+
+	Tourch(){
+
+	}
+
+	void display(){
+		noStroke();
+		for (int i = 0; i < c_radius; i+=distance){
+			fill(#FFFFFF, i/inner_radius);
+			ellipse(0, 0, c_radius - i, c_radius - i);
+		}
+	}
+};
